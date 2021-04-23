36109904b7e4c33c0825f097ed84e3e848211fa0
shabbir159 <ahadhusain321@gmail.com>
2021-04-23 22:51:58 +0530

    tutorial

---
+++ study/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import chapter
+from .models import topic
+from .models import sub_topic
+
+# Register your models here.
+admin.site.register(chapter)
+admin.site.register(topic)
+admin.site.register(sub_topic)
